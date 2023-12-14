7cfa3b21d6231ee11d16aea91976a757013d86c2
AdMatch-Chandana <chandana@admatch.io>
2023-12-14 10:08:02 +0530

    Added sample email content

--- src/index.js
+++ src/index.js
@@ -9,41 +9,59 @@
 const nodemailer = require('nodemailer');
 
 const { Logger, pino } = require('pino');
-const {gcpLogOptions}  = require('pino-cloud-logging');
+const { gcpLogOptions } = require('pino-cloud-logging');
 
 exports.SendEmail = (event, context) => {
 
+  let pubsubType = ""
+
   const logger = pino(gcpLogOptions(
     {
-        level: "info",
-        name: 'Sample Alert'
+      level: "info",
+      name: 'Sample Alert'
     }
-));
+  ));
 
-logger.info(event)
 
-const transporter = nodemailer.createTransport({
-  service: 'hotmail',
-  auth: {
-    user: process.env.USER,
-    pass: process.env.PASS
+  const decodedData = Buffer.from(event.data, 'base64').toString('utf-8');
+  const parsedData = JSON.parse(decodedData);
+
+  if (parsedData.status != 'FAILED') {
+    return;
   }
-});
 
-// Email content
-const mailOptions = {
-  from: process.env.USER,
-  to: process.env.EMAIL_RECEIVERS,
-  subject: process.env.SUBJECT,
-  text: process.env.CONTENT
-};
-
-// Send email
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.error(error);
+  if (parsedData.destinationDatasetId) {
+    pubsubType = "bqDataTransfer"
   } else {
-    console.log('Email sent: ' + info.response);
+    pubsubType = "s3ToGcs"
   }
-});
+
+  const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS
+    }
+  });
+
+  // Email content
+  const mailOptions = {
+    from: process.env.USER,
+    to: process.env.EMAIL_RECEIVERS,
+    subject: pubsubType === "bqDataTransfer" ? "Failed to Load data into bigquery" : "Failed to load data from S3 to GCS",
+    html: `
+    <p><b>Error Status:</b></p>
+    <p><b>Message:</b> ${parsedData.errorStatus.message}</p>
+    <p><b>Code:</b> ${parsedData.errorStatus.code}</p>
+  `
+  };
+
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 };
